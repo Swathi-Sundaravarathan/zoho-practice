d4b04d6521dec4de681d52f99021838072c4a17d
Swathi-Sundaravarathan <36847525+Swathi-Sundaravarathan@users.noreply.github.com>
2018-03-03 12:39:12 +0530

    Update two max numbers.c

--- two max numbers.c
+++ two max numbers.c
@@ -3,16 +3,25 @@
 int main()
 {
     int i,a[5]={10,20,30,40,5};
-    int max=a[0],max1=a[0],p;
-    
+    int max1=a[0],max2=a[1],p;
+    if(max2>max1)
+    {
+        p=max1;
+        max1=max2;
+        max2=p;
+    }
     for(i=0;i<5;i++)
     {
-        if((a[i]>max))
+        if((a[i]>max1))
         {
-            max1=max;
-            max=a[i];
+            max2=max1;
+            max1=a[i];
+        }
+        else if(a[i]>max2)
+        {
+            max2=a[i];
         }
     }
     
-    printf("%d %d",max,max1);
+    printf("%d %d",max1,max2);
 }
